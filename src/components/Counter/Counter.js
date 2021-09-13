8eab63b2cd757a76c189d5f3567251155aa49633
Az-s <azim.sa@yahoo.com>
2021-09-13 15:42:42 +0600

    complete counter component

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,38 +1,46 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './Counter.css';
-import {useDispatch, useSelector} from "react-redux";
-import {add, decrease, fetchCounter, increase, saveCounter, subtract} from "../../store/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { add, decrease, fetchCounter, increase, saveCounter, subtract, fetchPost } from "../../store/actions";
 
 
 const Counter = () => {
     const dispatch = useDispatch();
     const counter = useSelector(state => state.counter);
-  
+
     useEffect(() => {
-      dispatch(fetchCounter());
+        dispatch(fetchCounter());
     }, [dispatch]);
-  
+
     const increaseCounter = () => {
-      dispatch(increase());
-      // dispatch(saveCounter());
-    }
-    const addCounter = () => dispatch(add(5));
-    const decreaseCounter = () => dispatch(decrease());
-    const minusCounter = () => dispatch(subtract(5));
-  
-    // const save = () => {
-    //   dispatch(saveCounter(123));
-    // }
-  
+        dispatch(increase());
+        dispatch(fetchPost());
+    };
+    const addCounter = () => {
+        dispatch(add(5));
+        dispatch(fetchPost());
+    };
+    const decreaseCounter = () => {
+        dispatch(decrease());
+        dispatch(fetchPost());
+    };
+    const minusCounter = () => {
+        dispatch(subtract(5));
+        dispatch(fetchPost());
+    };
+    // const addCounter = () => dispatch(add(5));
+    // const decreaseCounter = () => dispatch(decrease());
+    // const minusCounter = () => dispatch(subtract(5));
+
     return (
-      <div className="Counter">
-        <h1>{counter}</h1>
-        <button onClick={increaseCounter}>Increase</button>
-        <button onClick={decreaseCounter}>Decrease</button>
-        <button onClick={addCounter}>Increase by 5</button>
-        <button onClick={minusCounter}>Decrease by 5</button>
-      </div>
+        <div className="Counter">
+            <h1>{counter}</h1>
+            <button onClick={increaseCounter}>Increase</button>
+            <button onClick={decreaseCounter}>Decrease</button>
+            <button onClick={addCounter}>Increase by 5</button>
+            <button onClick={minusCounter}>Decrease by 5</button>
+        </div>
     );
-  };
-  
-  export default Counter;
+};
+
+export default Counter;
--- src/store/actions.js
+++ src/store/actions.js
@@ -8,32 +8,53 @@ export const SUBTRACT = 'SUBTRACT';
 export const FETCH_COUNTER_REQUEST = 'FETCH_COUNTER_REQUEST';
 export const FETCH_COUNTER_SUCCESS = 'FETCH_COUNTER_SUCCESS';
 export const FETCH_COUNTER_FAILURE = 'FETCH_COUNTER_FAILURE';
+export const FETCH_COUNTER_POST = 'FETCH_COUNTER_POST';
 
-export const increase = () => ({type: INCREASE});
-export const decrease = () => ({type: DECREASE});
-export const add = value => ({type: ADD, payload: value});
-export const subtract = value => ({type: SUBTRACT, payload: value});
+export const TODO_SUCCESS = 'TO_DO_LIST_SUCCESS';
+export const POST_TODO = 'POST_TODOLIST';
+export const DELETE_TODO = 'DELETE_TODOLIST';
+export const KEEP_NEW_TEXT = 'KEEP_NEW_TEXT';
 
-export const fetchCounterRequest = () => ({type: FETCH_COUNTER_REQUEST});
-export const fetchCounterSuccess = counter => ({type: FETCH_COUNTER_SUCCESS, payload: counter});
-export const fetchCounterFailure = () => ({type: FETCH_COUNTER_FAILURE});
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+export const add = value => ({ type: ADD, payload: value });
+export const subtract = value => ({ type: SUBTRACT, payload: value });
+
+export const fetchCounterRequest = () => ({ type: FETCH_COUNTER_REQUEST });
+export const fetchCounterSuccess = counter => ({ type: FETCH_COUNTER_SUCCESS, payload: counter });
+export const fetchCounterFailure = () => ({ type: FETCH_COUNTER_FAILURE });
+export const fetchCounterPost = () => ({ type: FETCH_COUNTER_POST });
+
 
 export const fetchCounter = () => {
-  return async (dispatch, getState) => {
-    dispatch(fetchCounterRequest());
+    return async (dispatch, getState) => {
+        dispatch(fetchCounterRequest());
 
-    try {
-      const response = await axios.get('https://burgerjs1011-default-rtdb.firebaseio.com/counter.json');
+        try {
+            const response = await axios.get('https://az-sa-bd3f9-default-rtdb.firebaseio.com/counter.json');
 
-      if (response.data === null) {
-        dispatch(fetchCounterSuccess(0));
-      } else {
-        dispatch(fetchCounterSuccess(response.data));
-      }
-    } catch (e) {
-      dispatch(fetchCounterFailure());
-    }
-  };
+            if (response.data === null) {
+                dispatch(fetchCounterSuccess(0));
+            } else {
+                dispatch(fetchCounterSuccess(response.data));
+            }
+        } catch (e) {
+            dispatch(fetchCounterFailure());
+        }
+    };
+};
+
+export const fetchPost = () => {
+    return async (dispatch, getState) => {
+        dispatch(fetchCounterRequest());
+        const currentCounter = getState().counter;
+        try {
+            const response = await axios.put('https://az-sa-bd3f9-default-rtdb.firebaseio.com/counter.json' , {currentCounter});
+            dispatch(fetchCounterPost());
+        } catch (e) {
+            dispatch(fetchCounterFailure());
+        }
+    };
 };
 
 // export const saveCounter = () => {
